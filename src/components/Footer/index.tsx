b17e3b7471bf86cdfd1ff42417ec1103042b89e6
Vinícios <vviniciosc@gmail.com>
2023-11-24 04:19:48 -0300

    feat: created Footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+import { abrilFatface } from "@/app/layout";
+
+function Footer() {
+  return (
+    <footer className="flex flex-wrap basis-80 min-h-[10vh] bg-green-300">
+        <h3 className={`${abrilFatface.className} flex-grow basis-80`}>Contato Inteligente</h3>
+      <ul className="flex-grow basis-80">
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;

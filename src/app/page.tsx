c4316e4657850593b71cd416b2f61d2872955e54
Vinícios <vviniciosc@gmail.com>
2023-11-24 04:21:36 -0300

    feat: updated main page with basic components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
-import Image from 'next/image'
+import Footer from '@/components/Footer'
+import LeftColumn from '@/components/LeftColumn'
+import RightColumn from '@/components/RightColumn'
 
 export default function Home() {
   return (
     <>
+      <main className='flex flex-wrap'>
+        <LeftColumn />
+        <RightColumn />
+      </main>
+      <Footer />
     </>
   )
 }

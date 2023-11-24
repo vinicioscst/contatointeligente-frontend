5295cbf72592edb0b34c2819b13d72e9d1a68fcd
Vinícios <vviniciosc@gmail.com>
2023-11-24 04:20:28 -0300

    feat: configured page metadata and fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Abril_Fatface } from 'next/font/google'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
+export const abrilFatface = Abril_Fatface({ subsets: ['latin'], weight: ['400'] })
 
-export const metadata: Metadata = {
+const metadata: Metadata = {
   title: 'Contato Inteligente',
   description: 'Todos os seus contatos organizados em um único lugar',
 }

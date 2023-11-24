ac12ed36ae3de1b18de16b6dfbe1244c48d7a119
Vinícios <vviniciosc@gmail.com>
2023-11-24 19:35:28 -0300

    style: improved header style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,11 +4,11 @@ const Header = () => {
   return (
     <header className="w-full border-b border-text-color/20">
       <div className="flex justify-between w-full max-w-5xl p-6 my-0 mx-auto">
-        <Link href={"/"}><h5 className="text-xl font-bold text-accent-color hover:text-primary-color select-none cursor-pointer transition-colors">Contato Inteligente</h5></Link>
+        <Link href={"/"}><h5 className="text-xl font-bold hover:text-accent-color select-none cursor-pointer transition-colors">Contato Inteligente</h5></Link>
         <nav className="flex items-center gap-6 ">
-          <Link href={"#about-us"}><li className="cursor-pointer">Sobre nós</li></Link>
-          <Link href={"/register"}><li className="cursor-pointer">Cadastro</li></Link>
-          <Link href={"/login"}><li className="cursor-pointer">Faça seu login</li></Link>
+          <Link href={"#about-us"}><li className="hover:text-accent-color cursor-pointer transition-colors">Sobre nós</li></Link>
+          <Link href={"/register"}><li className="hover:text-accent-color cursor-pointer transition-colors">Cadastro</li></Link>
+          <Link href={"/login"}><li className="bg-primary-color hover:bg-accent-color py-2 px-4 rounded-md text-background-color hover:text-secondary-color cursor-pointer transition-colors">Faça seu login</li></Link>
         </nav>
       </div>
     </header>

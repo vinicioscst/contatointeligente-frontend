e6c0f1c2f084eecb35db5e7688944ffb3d03044d
Vinícios <vviniciosc@gmail.com>
2023-11-25 00:42:57 -0300

    style: fixed footer styles and fonts

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,7 @@
-import { abrilFatface } from "@/app/layout";
-
 function Footer() {
   return (
     <footer className="flex flex-wrap basis-80 min-h-[10vh] bg-green-300">
-        <h3 className={`${abrilFatface.className} flex-grow basis-80`}>Contato Inteligente</h3>
+        <h3 className='flex-grow basis-80'>Contato Inteligente</h3>
       <ul className="flex-grow basis-80">
         <li></li>
         <li></li>

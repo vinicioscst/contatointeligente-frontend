443c751245d0db69fc9d65b287a4ada19faa7c8a
Vinícios <vviniciosc@gmail.com>
2023-11-25 00:46:00 -0300

    feat: updated fonts and added header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,28 @@
-import type { Metadata } from 'next'
-import { Volkhov } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Volkhov } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
 
-const volkhov = Volkhov({ subsets: ['latin'], weight: ['400', '700'] })
+const volkhov = Volkhov({ subsets: ["latin"], weight: ["400", "700"] });
 
 const metadata: Metadata = {
-  title: 'Contato Inteligente',
-  description: 'Todos os seus contatos organizados em um único lugar',
-}
+  title: "Contato Inteligente",
+  description: "Todos os seus contatos organizados em um único lugar",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-BR">
-      <body className={`${volkhov.className} bg-background-color text-font-color scroll-smooth`}>{children}</body>
+      <body
+        className={`${volkhov.className} bg-background-color text-font-color scroll-smooth`}
+      >
+        <Header />
+        {children}
+      </body>
     </html>
-  )
+  );
 }

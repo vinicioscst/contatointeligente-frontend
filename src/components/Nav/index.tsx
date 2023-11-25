9d58ccaaaafa2ccf1d3b8b15701468ec2c570126
Vinícios <vviniciosc@gmail.com>
2023-11-25 14:22:22 -0300

    fix: fixed component pros and hover colors

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -8,12 +8,12 @@ const Nav = () => {
     <div className="hidden sm:block">
       <nav className="flex items-center gap-6">
         <Link href={"#about-us"}>
-          <li className="hover:text-accent-color cursor-pointer transition-colors">
+          <li className="hover:underline hover:text-background-color/80 underline-offset-4 cursor-pointer transition-all">
             Sobre nós
           </li>
         </Link>
         <Link href={"/register"}>
-          <li className="hover:text-accent-color cursor-pointer transition-colors">
+          <li className="hover:hover:underline hover:text-background-color/80 underline-offset-4 cursor-pointer transition-all">
             Cadastro
           </li>
         </Link>
@@ -24,7 +24,7 @@ const Nav = () => {
             type="button"
             hasImage={true}
           >
-            <LoginIcon stroke="#090d10" size="21" />
+            <LoginIcon stroke="#090d10" size={21} />
             Faça seu login
           </Button>
         </Link>

42abb1a4102ea3f45e00ea543a2b78d4ecc4871f
Vinícios <vviniciosc@gmail.com>
2023-11-25 00:44:21 -0300

    fix: added nav components and added responsiveness

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,27 @@
+"use client";
 import Link from "next/link";
+import Nav from "../Nav";
+import { useState } from "react";
+import MobileNav from "../MobileNav";
+import MenuIcon from "../icons/MenuIcon";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <header className="w-full border-b border-text-color/20">
-      <div className="flex justify-between w-full max-w-5xl p-6 my-0 mx-auto">
-        <Link href={"/"}><h5 className="text-xl font-bold hover:text-accent-color select-none cursor-pointer transition-colors">Contato Inteligente</h5></Link>
-        <nav className="flex items-center gap-6 ">
-          <Link href={"#about-us"}><li className="hover:text-accent-color cursor-pointer transition-colors">Sobre nós</li></Link>
-          <Link href={"/register"}><li className="hover:text-accent-color cursor-pointer transition-colors">Cadastro</li></Link>
-          <Link href={"/login"}><li className="bg-primary-color hover:bg-accent-color py-2 px-4 rounded-md text-background-color hover:text-secondary-color cursor-pointer transition-colors">Faça seu login</li></Link>
-        </nav>
+    <header className="w-full bg-primary-color">
+      <div className="flex gap-4 items-center justify-between w-full max-w-5xl p-4 my-0 mx-auto text-background-color">
+        <Link href={"/"}>
+          <h5 className="text-xl leading-none font-bold hover:tracking-wide select-none cursor-pointer transition-all">
+            Contato Inteligente
+          </h5>
+        </Link>
+        <Nav />
+        <div className="flex sm:hidden py-3" onClick={() => setOpen(!open)}>
+          <MenuIcon size="21" stroke="rgb(245 247 249)"/>
+        </div>
       </div>
+      {open === true && <MobileNav />}
     </header>
   );
 };

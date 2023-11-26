03d1e2e511c16463fecc8d4ae627f703def98af0
Vinícios <vviniciosc@gmail.com>
2023-11-26 19:39:04 -0300

    fix: removed unnecessary import

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Link from "next/link";
 import { useState } from "react";
-import MobileNav from "../MobileNav";
 import MenuIcon from "../icons/MenuIcon";
 
 interface IHeaderProps {

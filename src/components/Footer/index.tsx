48d0a37f7cbb9053fd819e46741430f50ee8122e
Vinícios <vviniciosc@gmail.com>
2023-11-27 09:38:49 -0300

    style: created footer style

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,12 @@
 function Footer() {
   return (
-    <footer className="flex flex-wrap basis-80 min-h-[10vh] bg-green-300">
-        <h3 className='flex-grow basis-80'>Contato Inteligente</h3>
-      <ul className="flex-grow basis-80">
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
+    <footer className="flex justify-center bg-secondary-color">
+      <div className="w-full max-w-5xl py-10 px-6 flex flex-col xm:flex-row gap-5 items-center justify-between">
+        <h6 className="text-lg leading-none font-bold select-none">
+          Contato Inteligente
+        </h6>
+        <p className="text-sm select-none">© 2023 Contato Inteligente</p>
+      </div>
     </footer>
   );
 }

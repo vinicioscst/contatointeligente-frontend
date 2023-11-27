ae7b01062025dcd5df70cccb220a8c8d3f70a625
Vinícios <vviniciosc@gmail.com>
2023-11-27 09:40:22 -0300

    feat: added dynamic onClick events to button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,17 +1,19 @@
-interface IButtonProps {
+interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   type: "button" | "submit" | "reset" | undefined;
   children: React.ReactNode;
   bgColor: string;
   textColor: string;
   hasImage?: boolean;
   borderColor: string;
+  clickEvent?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-function Button({ type, children, bgColor, textColor, hasImage, borderColor }: IButtonProps) {
+function Button({ type, children, bgColor, textColor, hasImage, borderColor, clickEvent }: IButtonProps) {
   return (
     <button
-      className={`${bgColor} ${textColor} ${hasImage === true && "flex gap-2 items-center"} outline outline-1 ${borderColor && `${borderColor}`} py-3 px-4 font-inter font-bold leading-none rounded-md hover:-translate-y-1 transition-transform`}
+      className={`${bgColor} ${textColor} ${hasImage === true && "flex gap-2 items-center"} outline outline-1 ${borderColor && `${borderColor}`} py-3 px-4 font-inter font-bold text-sm xm:text-base leading-none rounded-md hover:-translate-y-1 transition-transform`}
       type={type}
+      onClick={clickEvent}
     >
       {children}
     </button>
